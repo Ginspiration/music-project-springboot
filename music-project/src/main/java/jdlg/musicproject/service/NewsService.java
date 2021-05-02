5969aeb4df03d665415423e9ce72cdeac54599aa
kengdie571 <x822130209@qq.com>
2021-05-02 17:06:52 +0800

    新闻02：添加了service层，并修改了dao层中selectAllNew方法名，改为selectAllNews

--- music-project/src/main/java/jdlg/musicproject/service/NewsService.java
+++ music-project/src/main/java/jdlg/musicproject/service/NewsService.java
@@ -1,23 +1,22 @@
-package jdlg.musicproject.dao;
+package jdlg.musicproject.service;
 
 import jdlg.musicproject.entries.common.News;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
-public interface NewsDao {
+public interface NewsService {
 
     /* x:查询所有新闻 */
     List<News> selectAllNews();
 
     /* x:根据新闻标题查找新闻 */
-    List<News> selectNewByTitle(@Param("new_title")String newTitle);
+    List<News> selectNewByTitle(String newTitle);
 
     /* x:添加新闻 */
     int addNew(News news);
 
     /* x:根据新闻id删除新闻 */
-    int deleteNew(@Param("id")Integer id);
+    int deleteNew(int id);
 
     /* x:更新新闻 */
     int updateNew(News news);
---
+++ music-project/src/main/java/jdlg/musicproject/service/impl/NewsServiceImpl.java
@@ -0,0 +1,41 @@
+package jdlg.musicproject.service.impl;
+
+import jdlg.musicproject.dao.NewsDao;
+import jdlg.musicproject.entries.common.News;
+import jdlg.musicproject.service.NewsService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class NewsServiceImpl implements NewsService {
+
+    @Resource(name = "newsDao")
+    private NewsDao newsDao;
+
+    @Override
+    public List<News> selectAllNews() {
+        return newsDao.selectAllNews();
+    }
+
+    @Override
+    public List<News> selectNewByTitle(String newTitle) {
+        return newsDao.selectNewByTitle(newTitle);
+    }
+
+    @Override
+    public int addNew(News news) {
+        return newsDao.addNew(news);
+    }
+
+    @Override
+    public int deleteNew(int id) {
+        return newsDao.deleteNew(id);
+    }
+
+    @Override
+    public int updateNew(News news) {
+        return newsDao.updateNew(news);
+    }
+}
